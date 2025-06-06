8fb41e17efda9bc6ad9a54ceceeeb99e8fb6683d
baiel2019 <130566744+baiel2019@users.noreply.github.com>
2025-06-06 14:58:48 +0600

    add-fonts-colors-extansions

---
+++ invoice_app/lib/core/theme/app_colors.dart
@@ -0,0 +1,5 @@
+abstract class AppColors {
+  static const Color mainOrenge = Color(0xffFF4400);
+  static const Color greyButton = Color(0xff94A3B8);
+  static const Color greyText = Color(0xff7D81A3);
+}
