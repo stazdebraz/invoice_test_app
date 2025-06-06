da38947c3220c5c7476d088ba09fa06040d62717
Dastan Toktosunov <48437157+stazdebraz@users.noreply.github.com>
2025-06-06 14:46:29 +0600

    init_app

---
+++ invoice_app/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const InvoiceApp());
+}
+
+class InvoiceApp extends StatelessWidget {
+  const InvoiceApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
